4f9659580f3437f4f53e7c654b5668589db368c3
JayMar921 <jayharronabejar@gmail.com>
2023-02-20 21:17:38 +0800

    enabled swagger UI

--- AYUS-RestAPI.ASP/Program.cs
+++ AYUS-RestAPI.ASP/Program.cs
@@ -26,11 +26,15 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+/*
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+*/
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors(x => x
