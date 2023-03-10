f37e6801567db189d952af0dfa0dabf005e56fd0
JayMar921 <jayharronabejar@gmail.com>
2023-03-10 21:04:40 +0800

    New migration from accountstatus update

---
+++ AYUS-RestAPI.ASP/Migrations/20230308231618_migration9.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AYUS_RestAPI.ASP.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "accountStatus",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLocked",
+                table: "accountStatus",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOnline",
+                table: "accountStatus",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "accountStatus");
+
+            migrationBuilder.DropColumn(
+                name: "IsLocked",
+                table: "accountStatus");
+
+            migrationBuilder.DropColumn(
+                name: "IsOnline",
+                table: "accountStatus");
+        }
+    }
+}
