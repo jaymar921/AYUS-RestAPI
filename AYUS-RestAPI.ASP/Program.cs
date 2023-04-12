85c8649e27cbb23a082796311c9ddc8d9b43b345
JayMar921 <jayharronabejar@gmail.com>
2023-04-12 22:17:11 +0800

    Enabled SQLIte EF Core

--- AYUS-RestAPI.ASP/Program.cs
+++ AYUS-RestAPI.ASP/Program.cs
@@ -15,8 +15,8 @@ builder.Services.AddCors(options =>
 });
 
 // Add services to the container.
-
-builder.Services.AddDbContext<AppDbContext>( options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite($"Data Source=AYUS-DB"));
+//builder.Services.AddDbContext<AppDbContext>( options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<DataRepository>();
 builder.Services.AddSingleton<TempDataRepository>();
 
